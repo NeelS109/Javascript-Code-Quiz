fcfe78e900f588be58321949f3bf64be30ebccc2
Neel Sheth <nsheth109@gmail.com>
2021-02-17 21:22:18 -0500

    added event listener to go back to the index page

--- leaderboard.js
+++ leaderboard.js
@@ -8,3 +8,21 @@ clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 
+// Retreives local stroage 
+var totalScores = localStorage.getItem("allScores");
+totalScores = JSON.parse(totalScores);
+
+if (totalScores !== null) {
+
+    for (var i = 0; i < totalScores.length; i++) {
+
+        var oldHighscores = document.createElement("li");
+        oldHighscores.textContent = totalScores[i].Name + " " + totalScores[i].score;
+        highScore.appendChild(oldHighscores);
+
+    }
+}
+// Event listener to move to index page
+Back.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
