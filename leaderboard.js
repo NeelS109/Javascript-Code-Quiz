bab48d3ac980d4655872f4b76b2e2747a320bbcc
Neel Sheth <nsheth109@gmail.com>
2021-02-17 19:00:30 -0500

    added event listener to clear scores upon pressing clear

--- leaderboard.js
+++ leaderboard.js
@@ -2,3 +2,9 @@
 var highScore = document.querySelector("#leaderboard");
 var Back = document.querySelector("#Back");
 var clear = document.querySelector("#clear");
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+
